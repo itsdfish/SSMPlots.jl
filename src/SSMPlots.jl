c652d2ec67f507092f806a40968729fe83efe166
itsdfish <itsdfish@gmail.com>
2023-07-15 06:35:22 -0400

    add code

--- src/SSMPlots.jl
+++ src/SSMPlots.jl
@@ -1,5 +1,22 @@
 module SSMPlots
+    using SequentialSamplingModels
+    using SequentialSamplingModels: Approximate
+    using SequentialSamplingModels: Exact
+    using SequentialSamplingModels: get_pdf_type
+    using KernelDensity
+    using Plots 
 
-# Write your package code here.
+    import Plots: histogram
+    import Plots: histogram!
+    import Plots: plot
+    import Plots: plot! 
+
+    export histogram
+    export histogram!
+    export plot
+    export plot! 
+
+    include("kde.jl")
+    include("functions.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,44 @@
 using SSMPlots
 using Test
+using SequentialSamplingModels
 
 @testset "SSMPlots.jl" begin
-    # Write your tests here.
+    dist = RDM(;ν=[1,2,3], k=.30, A=.70, τ=.20)
+    h = histogram(dist)
+    plot!(h, dist)
+    
+    histogram(dist)
+    plot!(dist)
+    
+    plot(dist)
+    histogram!(dist)
+    
+    p = plot(dist)
+    histogram!(p, dist)
+    
+    dist = LCA()
+    p = plot(dist; t_range=range(.3, 1.2, length=100))
+    histogram!(p, dist)
+    
+    plot(dist; t_range=range(.3, 1.2, length=100))
+    histogram!(dist)
+    
+    h = histogram(dist)
+    plot!(h, dist; t_range=range(.3, 1.2, length=100))
+    
+    histogram(dist)
+    plot!(dist; t_range=range(.3, 1.2, length=100))
+    
+    dist = Wald(ν=3.0, α=.5, τ=.130)
+    h = histogram(dist)
+    plot!(h, dist; t_range=range(.130, 1.0, length=100))
+    
+    histogram(dist)
+    plot!(dist; t_range=range(.130, 1.0, length=100))
+    
+    p = plot(dist; t_range=range(.130, 1.0, length=100))
+    histogram!(p, dist)
+    
+    plot(dist; t_range=range(.130, 1.0, length=100))
+    histogram!(dist)
 end
